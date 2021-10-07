ebe34e9748bb099053347a34c20791b814dad0b0
DerekFurstPitt <drf57@pitt.edu>
2021-10-07 17:05:58 -0400

    initial commit. Establish file structure.

---
+++ src/hubmap_sdk/entity.py
@@ -0,0 +1,17 @@
+
+
+class entity_api:
+
+    @staticmethod
+    def create_entity(token, entityjson):
+        pass
+
+
+    @staticmethod
+    def update_entity(token, entityjson):
+        pass
+
+
+
+
+
