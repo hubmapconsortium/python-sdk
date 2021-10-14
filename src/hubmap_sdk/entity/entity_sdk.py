3e60034215c947d2c884b6f8eaf1e37a86c9c1b7
DerekFurstPitt <drf57@pitt.edu>
2021-10-14 16:41:31 -0400

    added tentative requirements document, implemented create_entity functionality. Creates an instance of one of the entity classes and returns.

--- src/hubmap_sdk/entity/entity_sdk.py
+++ src/hubmap_sdk/entity/entity_sdk.py
@@ -1,3 +1,151 @@
+from donor import donor
+from sample import sample
+from upload import upload
+from collection import collection
+from dataset import dataset
+import requests
+
 class entity_api:
-    def create_entity(self):
-        pass
+    def __init__(self, token=None, location=None):
+        self.token = token
+        self.location = location
+        if self.location.lower() == "prod" or self.location is None:
+            self.entity_url = "entity-api.hubmapconsortium.org/"
+        if self.location.lower == "test":
+            self.entity_url = "entity-api.test.hubmapconsortium.org/"
+        if self.location.lower() == "dev":
+            self.entity_url = "entity-api.dev.hubmapconsortium.org/"
+        if self.location.lower() == "stage":
+            self.entity._url = "entity-api.stage.hubmapconsortium.org/"
+        if self.location.lower() == "localhost":
+            self.entity_url = "localhost:8383"
+        else:
+            raise Exception("Argument 'location' if included must be case-insensitive: 'LOCALHOST', 'DEV', 'TEST', "
+                            "'STAGE', or 'PROD'. If not included, it will be assumed as 'PROD'")
+
+    def create_entity(self, entity_type, data):
+        header = {'Authorization': 'Bearer ' + self.token}
+        if entity_type.lower() not in ['donor, sample, dataset, upload, collection']:
+            raise Exception("Accepted entity types are (case-insensitive):" +
+                            " 'donor', 'sample', 'dataset', 'upload', or 'collection'")
+        url = self.entity_url + 'entities/' + entity_type
+        try:
+            r = requests.post(url, headers=header, json=data)
+        except Exception as e:
+            raise Exception(e)
+        if r.status_code < 400:
+            output = r.json
+            if entity_type.lower == "donor":
+                new_donor = donor(created_timestamp=output['created_timestamp'],
+                                  created_by_user_displayname=output['created_by_user_displayname'],
+                                  created_by_user_email=output['created_by_user_email'],
+                                  created_by_user_sub=output['created_by_user_sub'], uuid=output['uuid'],
+                                  hubmap_id=output['hubmap_id'],
+                                  last_modified_user_timestamp=output['last_modified_user_timestamp'],
+                                  last_modified_user_sub=output['last_modified_user_sub'],
+                                  last_modified_user_email=output['last_modified_user_email'],
+                                  last_modified_user_displayname=output['last_modified_user_displayname'],
+                                  entity_type= output['entity_type'], registered_doi=output['registered_doi'],
+                                  doi_url=output['doi_url'], creators=output['creators'], contacts=output['contacts'],
+                                  description=output['description'], data_access_level=output['data_access_level'],
+                                  image_files=output['image_files'], image_files_to_add=output['image_files_to_add'],
+                                  image_files_to_remove=output['image_files_to_remove'],
+                                  protocol_url=output['protocol'], metadata=output['metadata'],
+                                  submission_id=output['submission_id'], lab_donor_id=output['lab_donor_id'],
+                                  group_uuid=output['group_uuid'], group_name=output['group_name'],
+                                  label=output['label'])
+                return new_donor
+            if entity_type.lower() == "dataset":
+                new_dataset = dataset(created_timestamp=output['created_timestamp'],
+                                      created_by_user_displayname=output['created_by_user_displayname'],
+                                      created_by_user_email=output['created_by_user_email'],
+                                      created_by_user_sub=output['created_by_user_sub'], uuid=output['uuid'],
+                                      hubmap_id=['hubmap_id'],
+                                      last_modified_user_timestamp=output['last_modified_user_timestamp'],
+                                      last_modified_user_email=output['last_modified_user_email'],
+                                      last_modified_user_sub=output['last_modified_user_sub'],
+                                      last_modified_user_displayname=output['last_modified_user_displayname'],
+                                      entity_type=output['entity_type'], registered_doi=output['registered_doi'],
+                                      doi_url=output['doi_url'], creators=output['creators'],
+                                      contacts=output['contact'], antibodies=output['antibodies'],
+                                      description=output['description'], data_access_level=output['data_access_level'],
+                                      contains_human_genetic_sequences=output['contains_human_genetic_sequences'],
+                                      status=output['status'], title=output['title'], data_types=output['data_types'],
+                                      upload=output['upload'], collections=output['collections'],
+                                      contributors=output['contributors'], direct_ancestors=output['direct_ancestors'],
+                                      published_timestamp=output['published_timestamp'],
+                                      published_user_displayname=output['published_user_displayname'],
+                                      published_user_sub=output['published_user_sub'],
+                                      published_user_email=output['published_user_email'],
+                                      ingest_metadata=output['ingest_metadata'],
+                                      local_directory_rel_path=output['local_directory_rel_path'],
+                                      group_uuid=output['group_uuid'], group_name=output['group_name'],
+                                      previous_revision_uuid=output['previous_revision_uuid'],
+                                      next_revision_uuid=output['next_revision_uuid'],
+                                      thumbnail_file=output['thumbnail_file'],
+                                      thumbnail_file_to_add=output['thumbnail_file_to_add'],
+                                      thumbnail_file_to_remove=output['thumbnail_file_to_remove'])
+                return new_dataset
+            if entity_type.lower() == 'sample':
+                new_sample = sample(created_timestamp=output['created_timestamp'],
+                                    created_by_user_displayname=output['created_by_user_displayname'],
+                                    created_by_user_email=output['created_by_user_email'],
+                                    created_by_user_sub=output['created_by_user_sub'], uuid=output['uuid'],
+                                    hubmap_id=output['hubmap_id'],
+                                    last_modified_user_displayname=output['last_modified_user_displayname'],
+                                    entity_type=output['entity_type'],
+                                    registered_doi=output['registered_doi'], doi_url=output['doi_url'],
+                                    creators=output['creators'], contacts=output['contacts'],
+                                    description=output['description'], data_access_level=output['data_access_level'],
+                                    specimen_type=output['specimen_type'],
+                                    specimen_type_other=output['specimen_type_other'],
+                                    protocol_url=output['protocol_url'], group_uuid=output['group_uuid'],
+                                    group_name=output['group_name'], organ=output['organ'],
+                                    organ_other=output['organ_other'],
+                                    direct_ancestor_uuid=output['direct_ancestor_uuid'],
+                                    submission_id=output['submission_id'],
+                                    lab_tissue_sample_id=output['lab_tissue_sample_id'],
+                                    rui_location=output['rui_location'], metadata=output['metadata'],
+                                    visit=output['visit'], image_files=output['image_files'],
+                                    image_files_to_add=output['image_files_to_add'],
+                                    metadata_files=output['metadata_files'],
+                                    metadata_files_to_add=output['metadata_files_to_add'],
+                                    metadata_files_to_remove=output['metadata_files_to_remove'])
+                return new_sample
+            if entity_type.lower() == 'upload':
+                new_upload = upload(created_timestamp=output['created_timestamp'],
+                                    created_by_user_displayname=output['created_by_user_displayname'],
+                                    created_by_user_email=output['created_by_user_email'],
+                                    created_by_user_sub=output['created_by_user_sub'],uuid=output['uuid'],
+                                    hubmap_id=output['hubmap_id'],
+                                    last_modified_user_timestamp=output['last_modified_user_timestamp'],
+                                    last_modified_user_sub=output['last_modified_user_sub'],
+                                    last_modified_user_email=output['last_modified_user_email'],
+                                    last_modified_user_displayname=output['last_modified_user_displayname'],
+                                    entity_type=output['entity_type'], description=output['description'],
+                                    title=output['title'], status=output['status'],
+                                    validation_message=output['validation_message'], group_uuid=output['group_uuid'],
+                                    group_name=output['group_name'],
+                                    dataset_uuids_to_link=output['dataset_uuids_to_link'],
+                                    dataset_uuids_to_unlink=output['dataset_uuids_to_unlink'],
+                                    datasets=output['datasets'])
+                return new_upload
+            if entity_type.lower() == 'collection':
+                new_collection = collection(created_timestamp=output['created_timestamp'],
+                                            created_by_user_displayname=output['created_by_user_displayname'],
+                                            created_by_user_email=output['created_by_user_email'],
+                                            created_by_user_sub=output['created_by_user_sub'], uuid=output['uuid'],
+                                            hubmap_id=output['hubmap_id'],
+                                            last_modified_user_timestamp=output['last_modified_user_timestamp'],
+                                            last_modified_user_sub=output['last_modified_user_sub'],
+                                            last_modified_user_email=output['last_modified_user_email'],
+                                            last_modified_user_displayname=output['last_modified_user_displayname'],
+                                            datasets=output['datasets'], entity_type=output['entity_type'],
+                                            registered_doi=output['registered_doi'], doi_url=output['doi_url'],
+                                            creators=output['creators'], contacts=output['contacts'],
+                                            title=output['title'])
+                return new_collection
+        else:
+            err = r.json['error']
+            raise Exception(err)
+
