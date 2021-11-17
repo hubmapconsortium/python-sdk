357e4b9832bdd6f9b1cd91100cf6ec09b9732d22
DerekFurstPitt <drf57@pitt.edu>
2021-11-17 14:52:09 -0500

    fixed bug in inti block where token should have defaulted to None

--- src/hubmap_sdk/searchsdk.py
+++ src/hubmap_sdk/searchsdk.py
@@ -5,14 +5,15 @@ from hubmap_sdk import sdk_helper
 
 class SearchSdk:
 
-    def __init__(self, token, service_url=#https://search-api.hubmapconsortium.org/):
+    def __init__(self, token=None, service_url=#https://search-api.hubmapconsortium.org/):
                  ''):
         self.token = token
         if service_url.endswith('/'):
             self.search_url = service_url
         else:
             self.search_url = service_url + '/'
-        self.header = {'Authorization': 'Bearer ' + self.token}
+        if token is not None:
+            self.header = {'Authorization': 'Bearer ' + self.token}
 
     def assaytype(self, key):
         url = f"{self.search_url}assaytype"
