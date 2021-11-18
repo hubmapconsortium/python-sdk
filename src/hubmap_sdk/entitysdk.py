4a114a79f338d4a9bb04d5334e8bec5ca52ae1a4
DerekFurstPitt <drf57@pitt.edu>
2021-11-18 12:05:14 -0500

    completely refactored "identification" to "identifier"

--- src/hubmap_sdk/entitysdk.py
+++ src/hubmap_sdk/entitysdk.py
@@ -61,8 +61,8 @@ class EntitySdk:
     # the given sample or Dataset. This method does not require authorization, however if a token is given, it must be
     # valid, and if no token is given or if the token does not belong to HuBMAP-Read group, ancestor organ info will
     # only be returned for public entities.
-    def get_ancestor_organs(self, identification):
-        url = f"{self.entity_url}entities/{identification}/ancestor-organs"
+    def get_ancestor_organs(self, identifier):
+        url = f"{self.entity_url}entities/{identifier}/ancestor-organs"
         output = sdk_helper.make_request('get', self, url)
         organs_list = []
         for item in output:
@@ -72,8 +72,8 @@ class EntitySdk:
 
     # takes the id of an entity (HuBMAP ID or UUID) and returns an instance of the entity corresponding to the ID given
     # This method requires a token.
-    def get_entity_by_id(self, identification):
-        url = f"{self.entity_url}entities/{identification}"
+    def get_entity_by_id(self, identifier):
+        url = f"{self.entity_url}entities/{identifier}"
         output = sdk_helper.make_request('get', self, url)
         new_instance = sdk_helper.make_entity(output)
         return new_instance
@@ -81,8 +81,8 @@ class EntitySdk:
     # Takes in an id (HuBMAP ID or UUID) and returns a dictionary with the provenance tree above the given ID.
     # Optionally accepts an integer "depth" which will limit the size of the returned tree. This method requires a
     # token
-    def get_entity_provenance(self, identification, depth=None):
-        url = f"{self.entity_url}entities/{identification}/provenance"
+    def get_entity_provenance(self, identifier, depth=None):
+        url = f"{self.entity_url}entities/{identifier}/provenance"
         depth = f"?depth={depth}"
         output = sdk_helper.make_request('get', self, url, depth)
         return output
@@ -109,8 +109,8 @@ class EntitySdk:
     # Takes an id (HuBMAP ID or UUID) for a collection. Returns the details of the collection in the form of a
     # dictionary with the attached datasets. If no token, or a valid token with no HuBMAP-Read group membership, then
     # only a public collection will be returned, as well as only public datasets.
-    def get_collection(self, identification):
-        url = f"{self.entity_url}collections/{identification}"
+    def get_collection(self, identifier):
+        url = f"{self.entity_url}collections/{identifier}"
         output = sdk_helper.make_request('get', self, url)
         new_instance = Collection(output)
         return new_instance
@@ -142,8 +142,8 @@ class EntitySdk:
     # Updates the properties of a given entity. Accepts the id (HuBMAP ID or UUID) for the target entity to update, as
     # well as a dictionary with the new/updated properties for the entity. A token is required to update an entity. An
     # object is returned of the relevant class containing all the information of the entity.
-    def update_entity(self, identification, data):
-        url = f"{self.entity_url}entities/{identification}"
+    def update_entity(self, identifier, data):
+        url = f"{self.entity_url}entities/{identifier}"
         output = sdk_helper.make_request('put', self, url, data=data)
         new_instance = sdk_helper.make_entity(output)
         return new_instance
@@ -151,9 +151,9 @@ class EntitySdk:
     # Returns a list of all the ancestors of a given entity. Accepts an id (HuBMAP ID or UUID) for the target entity.
     # No token is required, however if a token is given, it must be valid. If no token is given or token is not for a
     # user in the Hubmap-Read group, ancestors will only be returned for public entities
-    def get_ancestors(self, identification):
+    def get_ancestors(self, identifier):
         list_of_ancestors = []
-        url = f"{self.entity_url}ancestors/{identification}"
+        url = f"{self.entity_url}ancestors/{identifier}"
         output = sdk_helper.make_request('get', self, url)
         for item in output:
             new_instance = sdk_helper.make_entity(item)
@@ -175,9 +175,9 @@ class EntitySdk:
     # Returns a list of all the parents of a given entity. Accepts an id (HuBMAP ID or UUID) for the target entity.
     # No token is required, however if a token is given, it must be valid. If no token is given or token is not for a
     # user in the Hubmap-Read group, parents will only be returned for public entities
-    def get_parents(self, identification):
+    def get_parents(self, identifier):
         list_of_parents = []
-        url = f"{self.entity_url}parents/{identification}"
+        url = f"{self.entity_url}parents/{identifier}"
         output = sdk_helper.make_request('get', self, url)
         for item in output:
             new_instance = sdk_helper.make_entity(item)
@@ -187,9 +187,9 @@ class EntitySdk:
     # Returns a list of all the parents of a given entity. Accepts an id (HuBMAP ID or UUID) for the target entity.
     # No token is required, however if a token is given, it must be valid. If no token is given or token is not for a
     # user in the Hubmap-Read group, parents will only be returned for public entities
-    def get_children(self, identification):
+    def get_children(self, identifier):
         list_of_children = []
-        url = f"{self.entity_url}children/{identification}"
+        url = f"{self.entity_url}children/{identifier}"
         output = sdk_helper.make_request('get', self, url)
         for item in output:
             new_instance = sdk_helper.make_entity(item)
@@ -199,9 +199,9 @@ class EntitySdk:
     # Returns a list of the previous revisions of a given entity. Accepts an id (HuBMAP ID or UUID) for the target
     # entity. No token is required, however if a token is given, it must be valid. If no token is given or token is not
     # for a user in the Hubmap-Read group, previous revisions will only be returned for public entities
-    def get_previous_revisions(self, identification):
+    def get_previous_revisions(self, identifier):
         list_of_previous_revisions = []
-        url = f"{self.entity_url}previous_revisions/{identification}"
+        url = f"{self.entity_url}previous_revisions/{identifier}"
         output = sdk_helper.make_request('get', self, url)
         for item in output:
             new_instance = sdk_helper.make_entity(item)
@@ -211,8 +211,8 @@ class EntitySdk:
     # Returns a list of the next revisions of a given entity. Accepts an id (HuBMAP ID or UUID) for the target
     # entity. No token is required, however if a token is given, it must be valid. If no token is given or token is not
     # for a user in the Hubmap-Read group, next revisions will only be returned for public entities
-    def get_next_revisions(self, identification):
-        url = f"{self.entity_url}next_revisions/{identification}"
+    def get_next_revisions(self, identifier):
+        url = f"{self.entity_url}next_revisions/{identifier}"
         output = sdk_helper.make_request('get', self, url)
         new_instance = Dataset(output)
         return new_instance
@@ -220,25 +220,25 @@ class EntitySdk:
     # Accepts an id (HuBMAP ID or UUID) for a collection and a list of datasets. Links each dataset in the list to the
     # target collection. Requires a valid Token. Returns a string "Successfully added all the specified datasets to the
     # target collection" if successful.
-    def add_datasets_to_collection(self, identification, list_of_datasets):
+    def add_datasets_to_collection(self, identifier, list_of_datasets):
         dataset_dictionary = {'dataset_uuids': list_of_datasets}
-        url = f"{self.entity_url}collections/{identification}/add-datasets"
+        url = f"{self.entity_url}collections/{identifier}/add-datasets"
         sdk_helper.make_request('put', self, url, data=dataset_dictionary)
         return "Successfully added all the specified datasets to the target collection"
 
     # Returns the globus url for an entity given by an id (HuBMAP ID or UUID). A token is not required, but if one is
     # given it must be valid. If a token is not given, or if the user does not have HuBMAP-Read group access, a globus
     # url will only be returned for public entities
-    def get_globus_url(self, identification):
-        url = f"{self.entity_url}entities/{identification}/globus-url"
+    def get_globus_url(self, identifier):
+        url = f"{self.entity_url}entities/{identifier}/globus-url"
         r = sdk_helper.make_request('get', self, url)
         return r
 
     # Returns a dataset object corresponding to the most recent revision of the dataset given by the id (HuBMAP ID or
     # UUID). A token is not required, but if one is provided, it must be valid. If a token is not given, or if the user
     # does not have HuBMAP-Read group access, then the last published dataset will be returned.
-    def get_dataset_latest_revision(self, identification):
-        url = f"{self.entity_url}datasets/{identification}/latest-revision"
+    def get_dataset_latest_revision(self, identifier):
+        url = f"{self.entity_url}datasets/{identifier}/latest-revision"
         output = sdk_helper.make_request('get', self, url)
         new_dataset = Dataset(output)
         return new_dataset
@@ -248,8 +248,8 @@ class EntitySdk:
     # dataset, it will return 2, and so on. A token is not required, however if a token is provided it must be valid.
     # If there is no token or the user does not have HuBMAP-Read group access, and the ID is for an unpublished dataset,
     # an error will be raised.
-    def get_dataset_revision_number(self, identification):
-        url = f"{self.entity_url}datasets/{identification}/revision"
+    def get_dataset_revision_number(self, identifier):
+        url = f"{self.entity_url}datasets/{identifier}/revision"
         output = sdk_helper.make_request('get', self, url)
         return output
 
@@ -257,9 +257,9 @@ class EntitySdk:
     # required and the user must have HuBMAP-Data-Admin access. Adds retraction reason as a property to the dataset, and
     # ads a property sub_status which is set to "retracted". Returns an dataset object for the given id with the new
     # properties.
-    def retract_dataset(self, identification, retraction_reason):
+    def retract_dataset(self, identifier, retraction_reason):
         retract_json = {'retraction_reason': retraction_reason}
-        url = f"{self.entity_url}datasets/{identification}/retract"
+        url = f"{self.entity_url}datasets/{identifier}/retract"
         output = sdk_helper.make_request('put', self, url, data=retract_json)
         new_dataset = Dataset(output)
         return new_dataset
@@ -271,11 +271,11 @@ class EntitySdk:
     # token is given it must be valid. If no token is given, or if the user does not have HuBMAP-Read group access, only
     # public datasets will be returned. If the id given itself is not public, and a token with read access is not given,
     # an error will be raised.
-    def get_revisions_list(self, identification, include_dataset=False):
+    def get_revisions_list(self, identifier, include_dataset=False):
         list_of_revisions = []
         if include_dataset != True:
             include_dataset = False
-        url = f"{self.entity_url}datasets/{identification}/revisions"
+        url = f"{self.entity_url}datasets/{identifier}/revisions"
         dataset_include = ''
         if include_dataset:
             dataset_include = '?include_dataset=True'
@@ -296,9 +296,9 @@ class EntitySdk:
     # Returns a list of all associated organs from a given id (HuBMAP ID or UUID). Does not require a token, however if
     # a token is given, it must be valid. If no token is given, or no HuBMAP-Read group access, only public datasets
     # will be accepted, and only public organs will be returned
-    def get_associated_organs_from_dataset(self, identification):
+    def get_associated_organs_from_dataset(self, identifier):
         list_or_organs = []
-        url = f"{self.entity_url}datasets/{identification}/organs"
+        url = f"{self.entity_url}datasets/{identifier}/organs"
         output = sdk_helper.make_request('get', self, url)
         for item in output:
             new_instance = Dataset(item)
